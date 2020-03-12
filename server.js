c3b460e448ee91c408c1d1cb5e882cbfd3dce64f
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-12 14:05:14 -0400

    m

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connectionString});
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5432; 
