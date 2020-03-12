7287ea42528284fc98e2edb4cfe95587ee1ded33
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-12 12:44:57 -0400

    m

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5432; 
 
 // tell it to use the public directory as one where static files live
 app.use(express.static(__dirname + '/public'));
