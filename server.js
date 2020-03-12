3123b4507095c94aaea1b52c6c69bd04572eacc6
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-12 09:53:54 -0400

    m

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var express = require('express')
-var app = express()
+// load the things we need
+var express = require('express');
+var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+app.get('/hello', function (req, res) {
+    res.send('hello world')
+  })
+
+app.listen(8080);
+console.log('8080 is the magic port');
