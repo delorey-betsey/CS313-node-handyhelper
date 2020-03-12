c0b7f7980a1567908ea81357f94debe81540496c
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-12 10:43:03 -0400

    m

--- index.js
+++ index.js
@@ -20,25 +20,63 @@ app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
 
+//______________________________________________________
 
-function sayHello(req,res) {
-	console.log("Received a request for: " + req.url);
-    var request = req.url;
-	if (request == '/home') 
-		{
-		  res.write("Welcome to the Home Page");
-		  res.end();
-		} else
-	if (request == '/getData') 
-		{
-		  res.write("Name:  Sister Delorey" + "\n");
-		  res.write("Class: CS313-03");
-		  res.end();
-		}  
-		else
-		{
-		  res.write("404:Page Not Found");
-		  res.end();
-		}  		
+function handleMath(request, response) {
+	const operation = request.query.operation;
+	const operand1 = Number(request.query.operand1);
+	const operand2 = Number(request.query.operand2);
+
+	// TODO: Here we should check to make sure we have all the correct parameters
+
+	computeOperation(response, operation, operand1, operand2);
 }
+
+function computeOperation(response, op, left, right) {
+	op = op.toLowerCase();
+
+	let result = 0;
+
+	if (op == "add") {
+		result = left + right;
+	} else if (op == "subtract") {
+		result = left - right;		
+	} else if (op == "multiply") {
+		result = left * right;
+	} else if (op == "divide") {
+		result = left / right;
+	} else {
+		// It would be best here to redirect to an "unknown operation"
+		// error page or something similar.
+	}
+
+	// Set up a JSON object of the values we want to pass along to the EJS result page
+	const params = {operation: op, left: left, right: right, result: result};
+
+	// Render the response, using the EJS page "result.ejs" in the pages directory
+	// Makes sure to pass it the parameters we need.
+	response.render('result', params);
+
+}
+
+// function sayHello(req,res) {
+// 	console.log("Received a request for: " + req.url);
+//     var request = req.url;
+// 	if (request == '/home') 
+// 		{
+// 		  res.write("Welcome to the Home Page");
+// 		  res.end();
+// 		} else
+// 	if (request == '/getData') 
+// 		{
+// 		  res.write("Name:  Sister Delorey" + "\n");
+// 		  res.write("Class: CS313-03");
+// 		  res.end();
+// 		}  
+// 		else
+// 		{
+// 		  res.write("404:Page Not Found");
+// 		  res.end();
+// 		}  		
+// }
 
