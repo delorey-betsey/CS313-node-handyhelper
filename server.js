ba3c367445c2b6b570d49bd6b19198ef1e4ac79a
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-12 09:55:09 -0400

    m

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/hello', function (req, res) {
+app.get('/', function (req, res) {
     res.send('hello world')
   })
 
