4b48ce888062050350541592756c39234e54df39
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-19 10:13:17 -0400

    m

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
 const { Pool } = require("pg");  
@@ -8,7 +9,10 @@ const connectionString = process.env.DATABASE_URL || "postgres://kyqsvxntadtknr:
 const pool = new Pool({connectionString: connectionString});
 
 app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.json()); // support json encoded bodies
+//app.use(express.urlencoded({extended: true})); // support url encoded bodies
 
 app.get('/getPerson', getPerson);
 
