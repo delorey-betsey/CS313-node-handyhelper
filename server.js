47f399daef0f39cecda12706b637c57a9e709381
Betsey Delorey <byuideloreyb@gmail.com>
2020-03-27 13:30:28 -0400

    m

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
+const cool = require('cool-ascii-faces');
 const express = require('express')
 const app = express();
 const path = require('path')
-
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
+
+const { Pool } = require("pg"); 
+const connectionString = process.env.DATABASE_URL || "postgres://xeuolzgrfowtaq:6b8c8e22e66da68f1ef2e992c44f8c484f2a2f85ee874ad6f182ff673d27808b@ec2-18-206-84-251.compute-1.amazonaws.com:5432/d49k598khgjq6h";
+//postgres://xeuolzgrfowtaq:6b8c8e22e66da68f1ef2e992c44f8c484f2a2f85ee874ad6f182ff673d27808b@ec2-18-206-84-251.compute-1.amazonaws.com:5432/d49k598khgjq6h
+const pool = new Pool({connectionString: connectionString,ssl:true});
 
 // express()
   app.use(express.static(path.join(__dirname, 'public')))
@@ -13,18 +19,81 @@ const PORT = process.env.PORT || 5000
   app.set('view engine', 'ejs')
 
   app.get('/', (req, res) => res.render('landing'));
-  app.post('/login', function(res, req) {
-    console.log("username: " + username +  " password: " + password )
-  });
-  app.post('/logout', function(res, req) {  
-  });
-  app.post('/getServerTime', function(res, req){
-  });
-  
-  app.post('/addNewRecipe', function(req, res){
-  });
+  app.get("/cool", (req, res) => res.send(cool()));
+  app.get('/addNewRecipe', (req, res) => res.render('addNewRecipe'));
+  app.get('/displayAllChefs', displayAllChefs);
+  app.get('/displayAllRecipes', displayAllRecipes);
 
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//________________DISPLAY ALL CHEFS___________________  
+//________________________________________________
+function displayAllChefs(request, response){
+
+    getAllChefsFromDb(function(error, result) {
+      // This is the callback function that will be called when the DB is done.
+      // The job here is just to send it back.
+  
+      // Make sure we got a row with the person, then prepare JSON to send back
+      if (error || result == null) {
+        response.status(500).json({success: false, data: error});
+      } else {
+        chefRows = JSON.stringify(result.rows);
+        response.render('displayAllChefs',{chefRows: chefRows});
+      }
+    });
+  }
+  function getAllChefsFromDb(callback) {
+    console.log("Getting chefs from DB");
+    var sql = "SELECT * FROM chefs";
+
+    pool.query (sql,function(err,result){
+      if (err) {
+        console.log("Error with select chefs database occurred. ")
+        console.log(err);
+        callback(err, null);
+      } 	// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+		callback(null, result);
+	});
+} //________________DISPLAY ALL RECIPES_________  
+//________________________________________________
+function displayAllRecipes(request, response){
+
+    getAllRecipesFromDb(function(error, result) {
+      // This is the callback function that will be called when the DB is done.
+      // The job here is just to send it back.
+  
+      // Make sure we got a row with the person, then prepare JSON to send back
+      if (error || result == null) {
+        response.status(500).json({success: false, data: error});
+      } else {
+        recipeRows = JSON.stringify(result.rows);
+        response.render('displayAllRecipes',{
+          recipeRows:   recipeRows
+          // title:        title, 
+          // instructions: instructions, 
+          // servings:     servings, 
+          // rnotes:       rnotes
+          });
+      }
+    });
+  }
+  function getAllRecipesFromDb(callback) {
+    console.log("Getting recipes from DB");
+    var sql = "SELECT * FROM recipes";
+
+    pool.query (sql,function(err,result){
+      if (err) {
+        console.log("Error with select recipes database occurred. ")
+        console.log(err);
+        callback(err, null);
+      } 	// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+		callback(null, result);
+	});
+}
+ 
 //___________________________________________________________________
 //_______ADD NEW RECIPE FUNCTION_________________________________________
 function addNewRecipe(req, res) {
